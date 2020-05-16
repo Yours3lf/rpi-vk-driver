1abeee4d14f5af3352871834dc37ec03643ed0ba
yours3lf <0.tamas.marton@gmail.com>
2020-05-16 10:27:04 +0100

    backup

--- QPUassembler/main.c
+++ QPUassembler/main.c
@@ -7,7 +7,7 @@
 
 int main()
 {
-#define shader singleTextureClippingPlane_AlphaDisabled_DstSrcColor_SrcZero_DepthStencilEnabled_FS
+#define shader singleTextureClippingPlane_AlphaGE80_DstOneMinusSrcAlpha_SrcSrcAlpha_DepthStencilEnabled_FS
 
 	for(uint32_t c = 0; c < sizeof(shader)/sizeof(uint64_t); ++c)
 	{
