06b0e9ac6af49b8b3c7f7c3b27b47f3e99971d5a
yours3lf <0.tamas.marton@gmail.com>
2020-06-18 17:51:18 +0100

    fixed install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 mkdir -p $HOME/.rpi-vk-driver/
-cp librpi-vk-driver.so $HOME/.rpi-vk-driver/
 mkdir -p $HOME/.local/share/vulkan/icd.d/
-cp rpi-vk-driver.json $HOME/.local/share/vulkan/icd.d/
+
+DRIVER=librpi-vk-driver.so
+JSON=rpi-vk-driver.json
+
+if [ -f "$DRIVER" ]; then
+	cp $DRIVER $HOME/.rpi-vk-driver/
+else
+	cp build/$DRIVER $HOME/.rpi-vk-driver/
+fi
+
+if [ -f "$JSON" ]; then
+	cp $JSON $HOME/.local/share/vulkan/icd.d/
+else
+	cp ../$JSON $HOME/.local/share/vulkan/icd.d/
+fi
